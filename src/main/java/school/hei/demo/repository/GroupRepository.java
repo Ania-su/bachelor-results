4dba93bd5d2d0bc6b1405d345fb35c3ba3c2b94c
Ania-su <antsaaa.randria@gmail.com>
2026-08-14 19:06:09 +0300

    feat: group repository

---
+++ src/main/java/school/hei/demo/repository/GroupRepository.java
@@ -0,0 +1,20 @@
+package school.hei.demo.repository;
+
+import java.util.UUID;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import school.hei.demo.repository.model.JGroup;
+
+public interface GroupRepository extends JpaRepository<JGroup, UUID> {
+
+    @Query("""
+      SELECT g FROM JGroup g
+      WHERE (:academicYear IS NULL OR g.academicYear = :academicYear)
+      """)
+    Page<JGroup> findAllFiltered(
+            @Param("academicYear") Integer academicYear,
+            Pageable pageable);
+}
