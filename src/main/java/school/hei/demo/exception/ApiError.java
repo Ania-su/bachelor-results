17a416d4903473da3519953c5c0b68d89ae46571
Ania-su <antsaaa.randria@gmail.com>
2026-08-14 11:22:35 +0300

    feat: rest controller advice exceptions handler

---
+++ src/main/java/school/hei/demo/exception/ApiError.java
@@ -0,0 +1,10 @@
+package school.hei.demo.exception;
+
+import java.time.Instant;
+
+public record ApiError(Instant timestamp, int status, String error, String message) {
+    public ApiError(int status, String error, String message) {
+        this(Instant.now(), status, error, message);
+    }
+}
+
