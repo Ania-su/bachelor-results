a3819dd698cd1e2b559724752f7a03ad72badd51
Ania-su <antsaaa.randria@gmail.com>
2026-08-14 14:28:52 +0300

    feat: course mappers

---
+++ src/main/java/school/hei/demo/endpoint/rest/controller/mapper/CourseRestMapper.java
@@ -0,0 +1,30 @@
+package school.hei.demo.endpoint.rest.controller.mapper;
+
+import org.springframework.stereotype.Component;
+import school.hei.demo.endpoint.rest.controller.dto.CourseRequest;
+import school.hei.demo.endpoint.rest.model.CourseResponse;
+import school.hei.demo.entity.Course;
+
+@Component
+public class CourseRestMapper {
+
+  public CourseResponse toResponse(Course course) {
+    if (course == null) return null;
+    return CourseResponse.builder()
+        .id(course.getId())
+        .reference(course.getReference())
+        .title(course.getTitle())
+        .semester(course.getSemester())
+        .credits(course.getCredits())
+        .build();
+  }
+
+  public Course toDomain(CourseRequest request) {
+    return Course.builder()
+        .reference(request.getReference())
+        .title(request.getTitle())
+        .semester(request.getSemester())
+        .credits(request.getCredits())
+        .build();
+  }
+}
