5d9eca4cce8bf7ce0d8a55bef5c4334ae1be9ee6
Ania-su <antsaaa.randria@gmail.com>
2026-08-16 17:50:33 +0300

    feat: exam group repository

---
+++ src/main/java/school/hei/demo/repository/ExamGroupRepository.java
@@ -0,0 +1,11 @@
+package school.hei.demo.repository;
+
+import java.util.List;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import school.hei.demo.repository.model.JExamGroup;
+
+public interface ExamGroupRepository extends JpaRepository<JExamGroup, UUID> {
+
+    List<JExamGroup> findAllByExam_Id(UUID examId);
+}
