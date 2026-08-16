0d6428017c40c342a5012701c645b22a41e9baa7
A. Ny Aina Nathan <nyaina.nathan.2@gmail.com>
2026-08-16 08:30:44 +0300

    feat: add grade entity crud endpoints

---
+++ src/main/java/school/hei/demo/repository/GradeRepository.java
@@ -0,0 +1,15 @@
+package school.hei.demo.repository;
+
+import java.util.UUID;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import school.hei.demo.repository.model.JGrade;
+
+public interface GradeRepository extends JpaRepository<JGrade, UUID> {
+  boolean existsByStudent_IdAndExam_Id(UUID studentId, UUID examId);
+
+  Page<JGrade> findAllByExam_Id(UUID examId, Pageable pageable);
+
+  Page<JGrade> findAllByExam_IdAndStudent_Id(UUID examId, UUID studentId, Pageable pageable);
+}
